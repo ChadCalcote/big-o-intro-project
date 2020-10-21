5488397546e46bc38d96e814ed262d36116c54b1
Chad Calcote <calcote@att.net>
2020-10-21 14:24:17 -0600

    Quadratic Fish Implement

--- 01-big-octopus.js
+++ 01-big-octopus.js
@@ -1,11 +1,30 @@
 function quadraticBiggestFish(fishes) {
-  // Code goes here ...
+  let longestFish = '';
+
+  for (let i = 0; i < fishes.length; i++) { // 'fish'
+
+    for (let j = 0; j < fishes.length; j++) {
+      if (fishes[i].length > fishes[j].length) {
+        console.log('J fish', fishes[j]);
+        console.log('I fish', fishes[i]);
+        // longestFish = fishes[i];
+      }
+    }
+  }
+
 }
+
+const fishies = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish',
+'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'];
+
+quadraticBiggestFish(fishies);
 
 
 function nlognBiggestFish(fishes) {
-  // Code goes here ...
+  
 }
+
+// O(n * log(n)) => loglinear
 
 
 function linearBiggestFish(fishes) {
@@ -32,3 +51,29 @@ tilesObj = {
 function fastDance(direction, tilesObj) {
   // Code goes here ...
 }
+
+/*
+
+Big O-ctopus and Biggest Fish
+Sluggish
+longest fish in O(n^2) time 
+compare all lengths to all other lengths
+
+Dominant
+Find longest in O(n log n) time
+Use Array.sort() method
+
+Clever
+O(n) time
+
+Dancing Octopus
+
+Slow Dance
+Iterate through array to return the tentacle number
+
+Constant Dance
+Access the tentacle number in O(1) time
+
+
+
+*/
