d19aef4b84d477c19e22c609e69a1e50c854a901
Michelle Yi <m7890yi@gmail.com>
2020-10-21 19:06:22 -0400

    third anagram

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -8,21 +8,36 @@ function firstAnagram(str1, str2) {
   return count === str2.length;
 }
 
-
 function secondAnagram(str1, str2) {
-  let ele1 = str1.split('').sort().join('');
-  let ele2 = str2.split('').sort().join('');
+  let ele1 = str1.split("").sort().join("");
+  let ele2 = str2.split("").sort().join("");
 
   return ele1 === ele2;
 }
 
 function thirdAnagram(str1, str2) {
-  // Code goes here ....
+  let str1Count = {};
+  let str2Count = {};
+  for (let i = 0; i < str1.length; i++) {
+    if (str1Count[str1[i]] === undefined && str2Count[str2[i]] === undefined) {
+      str1Count[str1[i]] = 1;
+      str2Count[str2[i]] = 1;
+    } else {
+      str1Count[str1[i]]++;
+      str2Count[str2[i]]++;
+    }
+  }
+  for (letter in str1Count) {
+    if (str1Count[letter] !== str2Count[letter]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function fourthAnagram(str1, str2) {
   // Code goes here ....
 }
 
-// console.log(secondAnagram("gizmo", "sally"));    // => false
-// console.log(secondAnagram("elvis", "lives"));    // => true
+console.log(thirdAnagram("gizmo", "sally")); // => false
+console.log(thirdAnagram("elvis", "lives")); // => true
