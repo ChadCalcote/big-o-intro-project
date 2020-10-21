588d8fa0c5b4c4cb716eb8458b8aa352ba884011
Michelle Yi <m7890yi@gmail.com>
2020-10-21 18:31:05 -0400

    anagram 1

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,18 +1,22 @@
 function firstAnagram(str1, str2) {
-  // Code goes here ....
+  let count = 0;
+  for (let i = 0; i < str1.length; i++) {
+    if (str2.indexOf(str1[i]) >= 0) {
+      count++;
+    }
+  }
+  return count === str2.length;
 }
-
-
+console.log(firstAnagram("elvis", "lives")); // true
+console.log(firstAnagram("gizmo", "sally")); // false
 
 function secondAnagram(str1, str2) {
   // Code goes here ....
 }
 
-
 function thirdAnagram(str1, str2) {
   // Code goes here ....
 }
-
 
 function fourthAnagram(str1, str2) {
   // Code goes here ....
