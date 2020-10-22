d78e959b756edb87c0fb38b3c44808640401944c
Michelle Yi <m7890yi@gmail.com>
2020-10-21 23:33:57 -0400

    bad two sum

--- 03-two-sum.js
+++ 03-two-sum.js
@@ -1,23 +1,30 @@
 function badTwoSum(arr, targetSum) {
-  // Code goes here ...
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === targetSum) return true;
+    }
+  }
+  return false;
 }
-
 
 function okayTwoSum1(arr, targetSum) {
   // Code goes here ...
 }
 
-
 function okayTwoSum2(arr, targetSum) {
   // Code goes here ...
 }
-
 
 function twoSum(arr, targetSum) {
   // Code goes here ...
 }
 
-
 function twoSumIndices(arr, targetSum) {
   // Code goes here ...
 }
+
+const arr = [0, 1, 5, 7];
+console.log(twoSum(arr, 6)); // => should be true
+console.log(twoSum(arr, 10)); // => should be falseconst arr = [0, 1, 5, 7];
+console.log(twoSum(arr, 6)); // => should be true
+console.log(twoSum(arr, 10)); // => should be false
